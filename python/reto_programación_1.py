4becb635f8d2f0d24ce73114b944b4c032c19ab6
Jossi10 <berdugojoseluiss@gmail.com>
2025-11-22 20:39:31 -0500

    Reset repository

--- python/reto_programación_1.py
+++
@@ -1,8 +0,0 @@
-import random
-
-dado = int(random.randint(1,6))
-
-if dado % 2 == 0:
-  print(f"El resultado del dado es: {dado}\nEl número es par")
-else:
- print(f"El resultado del dado es: {dado}\nEl número es impar")
